d9a75f66ce7537d71db643b2f9a5ee8203d2bcff
Ashika Kumar <ashkak1905@gmail.com>
2025-10-13 15:21:23 -0400

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)

6ff129c37678ebc0a1ee85364d8011d088014863
Lyanis Rubí Barreto Mercado <lyanis.barreto@gmail.com>
2025-10-31 03:20:01 -0400

    Updated styling for icons and for the button. I have added the functionality to the button, though more changes are going to have to be made

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,15 @@
 
 //calendar function
-function eventCalendar {
+function eventCalendar() {
     //insert calendar here
 }
 
 
 
 
-//see more dropdown here
-function openTab (element, tabname){
-    var addedList = document.getElementById('added-list')
-    var seeMore = document.getElementsByClassName('see-more')
 
-    seeMore.addEventListener('click', ()) => {
-        addedList.classList.toggle("inactive");
-        addedList.classList.toggle("active");
-    }
+function openTab() {
+    var addedList = document.getElementById('added-list');
+    addedList.classList.toggle("inactive");
+    addedList.classList.toggle("active");
 }

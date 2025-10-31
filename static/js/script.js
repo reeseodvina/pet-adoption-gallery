3d25428b5e8ed37134f6ba1d7c500023258dc095
Lyanis Rubí Barreto Mercado <lyanis.barreto@gmail.com>
2025-10-31 00:32:23 -0400

    Updated the "see more" button, and started progress on a possibe calendar

---
+++ static/js/script.js
@@ -0,0 +1,19 @@
+
+//calendar function
+function eventCalendar {
+    //insert calendar here
+}
+
+
+
+
+//see more dropdown here
+function openTab (element, tabname){
+    var addedList = document.getElementById('added-list')
+    var seeMore = document.getElementsByClassName('see-more')
+
+    seeMore.addEventListener('click', ()) => {
+        addedList.classList.toggle("inactive");
+        addedList.classList.toggle("active");
+    }
+}
